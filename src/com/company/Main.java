245b38c7eeb6157eb94963e3c9525eceb422b831
elenaBayt <elbaytime@gmail.com>
2021-12-15 18:02:29 -0500

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException{
+
+        for (int i=0; i<5; i++){
+            System.out.println("Batch 10");
+            Thread.sleep(2000);
+        }
+    }
+}
